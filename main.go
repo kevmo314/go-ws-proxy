ccd1176809360fd171f2ff0ad4ecad6d946f6b2f
Kevin Wang <kevmo314@gmail.com>
2025-03-13 12:36:10 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	"golang.org/x/net/websocket"
+)
+
+func main() {
+	origin := flag.String("origin", "http://localhost/", "Origin header to send to destination")
+	protocol := flag.String("protocol", "", "Protocol to use")
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		panic("Usage: go-ws-proxy <destination> [--origin <origin>] [--protocol <protocol>]")
+	}
+	dst := os.Args[1]
+
+	ws, err := websocket.Dial(dst, *protocol, *origin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
+		log.Fatal(err)
+	}
+	go io.CopyBuffer(ws, os.Stdin, make([]byte, ws.MaxPayloadBytes))
+	if _, err := io.CopyBuffer(ws, os.Stdin, make([]byte, ws.MaxPayloadBytes)); err != nil {
+		log.Fatal(err)
+	}
+}
