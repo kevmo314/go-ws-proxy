8eed136808d15ca93936c88a8e375bea074c2cb6
Kevin Wang <kevmo314@gmail.com>
2025-12-06 10:00:32 -0500

    feat: handle SIGINT and SIGTERM for graceful shutdown

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"golang.org/x/net/websocket"
 	"golang.org/x/term"
@@ -25,6 +27,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigc
+		ws.Close()
+	}()
+
 	go func() {
 		state, err := term.MakeRaw(int(os.Stdin.Fd()))
 		if err == nil {
